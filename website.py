2063fffa4dcaba20dc8893391b54f455f152c1d8
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-06-26 19:26:01 +0300

    merge with website

---
+++ website.py
@@ -0,0 +1,48 @@
+
+
+try:
+	with open("wordle-La.txt", "r") as wordList:
+		words = [line.strip() for line in wordList]
+
+	# print("Possible guesses: ")
+	# for word in words:
+	# 	print(word)
+
+except FileNotFoundError:
+	print("Error: Word list not found.")
+
+except PermissionError:
+	print("Error: No permissions to open the Word List.")
+
+except Exception as e:
+	print(f"Unexpected error: {e}")
+
+while (1):
+	print("Green letters")
+	green = input().strip().lower() #Green Typecasted to string
+	print("Yellow letters")
+	yellow = input().strip().lower() #Yellow Typecasted to string
+	print("Grey letters")
+	grey = input().strip().lower() #Gray Typecasted to string
+
+	answer = "hello"
+
+	print("--Input--")
+	print(green)
+	print(yellow)
+	print(grey)
+
+	if input == answer:
+		print("Congratulations!")
+	else:
+		filtered = [word for word in words if all(letter not in word for letter in grey)]
+		# filtered = [word for word in words if all(letter not in word[2] for letter in green)]
+		for i, letter in enumerate(green):
+			if letter != "0":
+				filtered = [word for word in filtered if word[i] == letter]
+		for i, letter in enumerate(yellow):
+			if letter != "0":
+				filtered = [word for word in filtered if letter in word and word[i] != letter]
+		print("Possible guesses: ")
+		print(" | ".join(filtered))
+		words = filtered
